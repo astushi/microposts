a8c3beef260be63d43da624d2dfbaf0d719f61d5
Atsushi KAWAMURA <astushi.k@gmail.com>
2016-01-11 10:55:51 +0000

    fix ver2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,11 @@
 class SessionsController < ApplicationController
   def new
   end
-<<<<<<< HEAD
-
-  def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-=======
   
   def create
 
     @user = User.find_by(email: params[:session][:email].downcase)
     
->>>>>>> user-profile
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:info] = "logged in as #{@user.name}"
@@ -21,18 +15,10 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> user-profile
   def destroy
     session[:user_id] = nil
     redirect_to root_path
   end
-<<<<<<< HEAD
-end
-=======
 
 end
->>>>>>> user-profile
