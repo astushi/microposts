4f8a903140c603fe665bcf6b72ad3bfeec96583e
Atsushi KAWAMURA <astushi.k@gmail.com>
2016-01-22 08:08:05 +0000

    fixed

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,15 @@
 class LikesController < ApplicationController
     
     def like
-        micropost = Micropost.find(params[:micropost_id])
-        like = current_user.likes.build(micropost_id: micropost.id)
+        @micropost = Micropost.find(params[:micropost_id])
+        like = current_user.likes.build(micropost_id: @micropost.id)
         like.save
         #redirect_to root_path
     end
     
     def unlike
-        micropost = Micropost.find(params[:micropost_id])
-        like = current_user.likes.find_by(micropost_id: micropost.id)
+        @micropost = Micropost.find(params[:micropost_id])
+        like = current_user.likes.find_by(micropost_id: @micropost.id)
         like.destroy
         #redirect_to root_path
     end
